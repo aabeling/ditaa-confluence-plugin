d4ec576e171b03ad58ed1dad3a214b17e7478dd9
aabeling <aabeling@1e2b662f-4d29-0410-a210-e5edf371d7ff>
2009-11-16 10:50:39 +0000

    uses the SettingsManager to get the base url now

--- src/main/java/de/banapple/confluence/DitaaMacro.java
+++ src/main/java/de/banapple/confluence/DitaaMacro.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.atlassian.confluence.setup.settings.SettingsManager;
 import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.BaseMacro;
@@ -15,7 +16,7 @@ public class DitaaMacro extends BaseMacro
 	private static Log log = LogFactory.getLog(DitaaMacro.class);
 	
 	private GridUrlCodec codec;
-	
+	private SettingsManager settingsManager;
 	
     public boolean isInline()
     {
@@ -38,20 +39,29 @@ public class DitaaMacro extends BaseMacro
     	log.debug("created");
     }
     
-    public String execute(Map params, String body, RenderContext renderContext)
+    @SuppressWarnings("unchecked")
+	public String execute(Map params, String body, RenderContext renderContext)
         throws MacroException
     {	
-    	System.out.println("macro.body="+body);
+    	final String METHOD = "execute: ";
+    	
+    	log.debug(METHOD+"macro.body="+body);
     	String encodedBody = codec.encode(body);
-    	System.out.println("macro.encodedBody="+encodedBody);
+    	log.debug(METHOD+"macro.encodedBody="+encodedBody);
         StringBuffer result=new StringBuffer();
         result.append("<img src=\"")
-        	.append("/confluence/plugins/servlet/ditaa?grid=")
+        	.append(settingsManager.getGlobalSettings().getBaseUrl())
+        	.append("/plugins/servlet/ditaa?grid=")
         	.append(encodedBody)
         	.append("\" />");
         
         return result.toString();
     }
+
+	public void setSettingsManager(SettingsManager settingsManager)
+	{
+		this.settingsManager = settingsManager;
+	}
 
     
 }
